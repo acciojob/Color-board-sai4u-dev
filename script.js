52cebc15fa7695fab06de8b7b332e5239e4b2d07
Sai Narendra <tannirusainarendra81@gmail.com>
2025-08-09 18:00:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-//your JS code here. If required.
+const container = document.getElementById("board");
+const SQUARES = 800;
+
+const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71'];
+
+function getRandomColor() {
+  return colors[Math.floor(Math.random() * colors.length)];
+}
+
+for (let i = 0; i < SQUARES; i++) {
+  const square = document.createElement('div');
+  square.classList.add('square');
+
+  square.addEventListener('mouseover', () => setColor(square));
+  square.addEventListener('mouseout', () => removeColor(square));
+
+  container.appendChild(square);
+}
+
+function setColor(element) {
+  const color = getRandomColor();
+  element.style.backgroundColor = color;
+}
+
+function removeColor(element) {
+  setTimeout(() => {
+    element.style.backgroundColor = '#1d1d1d';
+  }, 1000); // 1 second delay
+}
